ebbeafb5f2931c664b64f2fd079366daac5a6a72
Abigail002 <abigailameogno98@gmail.com>
2023-02-07 16:55:48 +0000

    Add of broadCast function

---
+++ src/chatserver/ChatServer.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chatserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**ChatServer and b
+ *
+ * @author user
+ */
+public class ChatServer extends Thread {
+
+    int nbClient = 0;
+
+    public static void main(String[] args) throws IOException {
+        new ChatServer().start();
+    }
+
+    @Override
+    public void run() {
+        int cpt = 5;
+
+        System.out.println("Démarrage du serveur...");
+        try {
+            ServerSocket ss = new ServerSocket(1234);
+            while (true) {
+                //Accept connections
+                Socket socket = ss.accept();
+                nbClient++;
+                Conversation conversation = new Conversation(socket, nbClient);
+                conversation.start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        while (cpt > 0) {
+            System.out.println("Bonjour le monde");
+            cpt--;
+        }
+    }
+
+    //Class Conversation inner ChatServer
+    private class Conversation extends Thread {
+
+        private int numClient;
+        private Socket socket;
+ 
+        public Conversation(Socket socket, int numClient) {
+            super();
+            this.socket = socket;
+            this.numClient = numClient;
+        }
+
+        @Override
+        public void run() {
+            try {
+                //Récupération d'un octet
+                InputStream is;
+                is = socket.getInputStream();
+                //Récupération d'un caractère
+                InputStreamReader isr = new InputStreamReader(is);
+                //Formation d'une chaine de caractères
+                BufferedReader br = new BufferedReader(isr);
+
+                //Flux en sortie
+                OutputStream os = socket.getOutputStream();
+                PrintWriter pw = new PrintWriter(os, true);//true permet l'affichage d'une donnée dès qu'on l'a
+                System.out.println("Connection du client " + numClient);
+
+                //Récupération de l'adresse ip
+                String IP = socket.getRemoteSocketAddress().toString();
+                pw.println("Connection du client numéro " + numClient + " et d'adresse ip " + IP);
+
+                //Récupération d'une chaine de caractère
+                while (true) {
+                    String req = br.readLine();
+                    if (req != null) {
+                        pw.println("Le client " + numClient + " a envoyé " + req);
+                        int longReq = req.length();
+                        pw.println("Longueur : " + longReq);
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
